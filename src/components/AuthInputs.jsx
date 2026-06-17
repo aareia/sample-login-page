e827fad45c8587ca60ae6a212647ea7176e3d484
aareia <ashton-77@hotmail.com>
2026-06-17 14:22:11 -0400

    Restyle AuthInputs component using tailwind

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -4,12 +4,12 @@ import { styled } from 'styled-components'
 import Button from './Button.jsx';
 import Input from './Input.jsx';
 
-const ContolContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`
+// const ContolContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   gap: 0.5rem;
+//   margin-bottom: 1.5rem;
+// `
 
 
 export default function AuthInputs() {
@@ -33,8 +33,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ContolContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 mx-auto rounded shadow-md bg-linear-to-b from-stone-700 to-stone-800'>
+      <div className='flex flex-col gap-2 mb-6'>
         <Input 
         invalid={emailNotValid} 
         label='Email' 
@@ -49,9 +49,9 @@ export default function AuthInputs() {
             handleInputChange('password', event.target.value)
           }
         />
-      </ContolContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4 actions">
+        <button type="button" className="text-amber-400 hover:text-amber-500 text-button">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
